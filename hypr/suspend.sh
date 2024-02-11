8c2932b59d5294b2c26c91c0ed852e06e65bb36f
Luke Kingland <luke@lukekingland.com>
2024-02-12 07:27:43 +0900

    lock bitwarden on swaylock

--- hypr/suspend.sh
+++ hypr/suspend.sh
@@ -6,4 +6,4 @@ swayidle -w \
   timeout 1200 ' hyprctl dispatch dpms off' \
   timeout 3600' systemctl suspend' \
   resume ' hyprctl dispatch dpms on' \
-  before-sleep 'swaylock -c000000'
+  before-sleep 'bw lock & swaylock -c000000'
